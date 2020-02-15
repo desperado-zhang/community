5b386ccf0de222d949e9233c27bd9050be08ab98
leo <2397648546@qq.com>
2020-02-15 20:54:55 +0800

    leo 我的问题页面分页完成

--- src/main/java/life/majiang/community/mapper/QuestionMapper.java
+++ src/main/java/life/majiang/community/mapper/QuestionMapper.java
@@ -13,11 +13,16 @@ public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modify,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModify},#{creator},#{tag})")
     void create(Question question);
 
-    @Select("select * from question limit #{offset}, #{size}")
+    @Select("select * from question  limit #{offset}, #{size} ")
     List<Question> list(@Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
 
     @Select("select count(*) from question")
     Integer count();
 
+    @Select("select * from question where creator = #{userId}  limit #{offset}, #{size} ")
+    List<Question> listByUserId(@Param("userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
+
+    @Select("select count(1) from question where creator = #{userId}")
+    Integer countByUserId(@Param("userId") Integer userId);
 }
 
